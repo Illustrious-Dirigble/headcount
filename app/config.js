b576fe35b7e5e4e3996e55da551ee80093a7a3fd
Matt Brown <mattfdbrown@gmail.com>
2015-05-04 16:35:06 -0700

    Fix path

--- app/config.js
+++ app/config.js
@@ -1,9 +1,6 @@
 var Bookshelf = require('bookshelf');
-var path = require('path');
-var local_knex = require(path.join(__dirname,'/local_config.js'));
 
-
-var knex =  !process.env.DATABASE_URL ? local_knex :
+var knex =  !process.env.DATABASE_URL ? require('./local_config.js') :
   require('knex')({
   client: 'pg',
   connection: process.env.DATABASE_URL
