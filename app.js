3412456a717868611610362e61b6bfaf61ad3d88
Matt Brown <mattfdbrown@gmail.com>
2015-05-04 10:48:35 -0700

    Remove EJS from code
    Removed files from views directory
    Removed EJS from package.json
    Removed views engine from app.js

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
