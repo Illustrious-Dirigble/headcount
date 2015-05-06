f77eabf5b719fcbd1726c74120955285973ff1f0
Prakal <pranav.89@gmail.com>
2015-05-05 17:44:59 -0700

    handle sign up and sign in within our auth router. passport-local works for signup. delete users folder in server side

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var User  = require('./../app/models/user');
 var oauth = require('../oauth.js');
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
@@ -26,6 +27,32 @@ router.get('/google/callback', passport.authenticate('google', { failureRedirect
 // Local Auth
 router.post('/local', passport.authenticate('local', { failureRedirect: '#/signup' }), function(req, res) {
  res.redirect('#/signin');
+});
+// local Auth signup
+router.post('/local-signup', function(req, res, next) {
+ console.log('signup');
+ var username  = req.body.username;
+ var password  = req.body.password;
+ 
+ new User({username:username})
+     .fetch()
+     .then(function(model){
+       if(model) {
+         next(new Error('User already exists'));
+       } else {
+         new User({username:username,password:password},{isNew:true}).save()
+                 .then(function(model){
+                   console.log(model);
+                   // var token = jwt.encode(model.attributes.username, 'secret');
+                   // res.json({token: token});
+                   console.log('New user saved');
+                   res.end('/links');
+                 });
+       }
+     })
+     .catch(function(error){
+       console.log('hi',error);
+     });
 });
 
 module.exports = router;
