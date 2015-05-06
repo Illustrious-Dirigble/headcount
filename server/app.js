87fb83f3e7dcc8d90e6ae3947d973eed09f6476a
Prakal <pranav.89@gmail.com>
2015-05-05 19:51:19 -0700

    progress in signin. shows whether username and password match.

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var routes       = require('./../routes/index');
 var users        = require('./../routes/users');
-var User        = require('./../app/models/user');
+var User        = require('../app/models/user');
 var auth         = require('./../routes/auth');
 var oauth        = require('./../oauth.js');
 var passport     = require('passport');
@@ -82,25 +82,38 @@ function(accessToken, refreshToken, profile, done) {
 
 // local Auth
 
-passport.use(new LocalStrategy(
+passport.use('local',new LocalStrategy({
+  // by default, it uses username and password
+  // usernameField : 'email',
+  // passwordField : 'password',
+},
   function(username, password, done) {
     console.log('passport local triggered',username);
-    console.log(new User({username:'ppo'}));
-    new User({ username: username }, function(err, user) {
+    console.log(new User({username:username}));
+    new User({ username: username })
+      .fetch()
+      .then(function(user) {
       console.log('user',user);
-      if (err) { return done(err); }
       if (!user) {
         console.log('incorrect username');
         return done(null, false, { message: 'Incorrect username.' });
       }
-      if (!user.validPassword(password)) {
-        console.log('incorrect [password]');
-        return done(null, false, { message: 'Incorrect password.' });
+      if (user.comparePassword(password,function(x){
+        // callback. x is the value of isMatch
+        console.log('isMatch',x);
+        if (x === true){
+          return done(null, user);
+        } else {
+          // wrong password
+          console.log('incorrect [password]');
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+      })){
+        return "TRUUUUUE";
       }
-      return done(null, user);
-    });
-  }
-));
+      // if (err) { return done(err); }
+    })
+  }));
 
 // error handlers
 
