e0f145874417caeca6cdd04fca87abd362864a3d
Khoa Phan <phankhoa.hack@gmail.com>
2015-05-06 00:20:11 -0700

    Fix sign-up and sign-in to successfully create users, login, and redirect to appropriate page

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var User  = require('./../app/models/user');
 var oauth = require('../oauth.js');
 var passport = require('passport');
+var bcrypt = require('bcrypt-nodejs');
 var FacebookStrategy = require('passport-facebook').Strategy;
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
@@ -26,34 +27,29 @@ router.get('/google/callback', passport.authenticate('google', { failureRedirect
 
 // Local Auth
 router.post('/local', passport.authenticate('local', { failureRedirect: '#/signup' }), function(req, res) {
-	console.log('success signin!!!!!!');
- 	res.redirect('#/links');
+ 	res.redirect('..#/links');
 });
+
 // local Auth signup
 router.post('/local-signup', function(req, res, next) {
- console.log('signup');
- var username  = req.body.username;
- var password  = req.body.password;
+  var username  = req.body.username;
+  var password  = req.body.password;
  
- new User({username:username})
-     .fetch()
-     .then(function(model){
-       if(model) {
-         next(new Error('User already exists'));
-       } else {
-         new User({username:username,password:password},{isNew:true}).save()
-                 .then(function(model){
-                   console.log(model);
-                   // var token = jwt.encode(model.attributes.username, 'secret');
-                   // res.json({token: token});
-                   console.log('New user saved');
-                   res.end('/links');
-                 });
-       }
-     })
-     .catch(function(error){
-       console.log('hi',error);
-     });
+  new User({username:username})
+    .fetch()
+    .then(function(model){
+      if (model) {
+        next(new Error('User already exists'));
+      } else {
+        new User({username:username, password:password}, {isNew:true}).save()
+          .then(function(model) {
+            res.end();
+          });
+        }
+      })
+    .catch(function(error){
+      console.log('hi',error);
+    });
 });
 
 module.exports = router;
