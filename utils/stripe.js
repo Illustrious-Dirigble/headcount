69dd901f735c33b491ddfc79a566c42c4cbbe2f9
Ansel Rosenberg <ansel01@gmail.com>
2015-05-07 23:54:01 -0700

    Add Stripe helper function file with customer creation from debit card entry
    Function takes token from returned by Stripe after client-side debit card submittal, returns customer saved to Stripe platform account for later use

---
+++ utils/stripe.js
@@ -0,0 +1,25 @@
+/**
+ * Require stripe module instance with platform account's secret API key
+ */
+var stripe = require("stripe")("sk_test_OBXX3FRuomYskOfEP62qbgMz"); 
+
+
+/**
+ * Takes token created from a debit card client side to create a 'customer' object associated with the Stripe platform account.
+ * Customer will later be retreived and charged to a different user's Stripe Connect account.
+ */
+exports.createPlatformCustomer = function(stripeToken, callback) {
+  stripe.customers.create({
+    source: stripeToken 
+  }, function(err, customer) {
+    // callback(err, customer)
+
+    if (err) {
+      console.log('Error:', err)
+    } else {
+      console.log('New customer!')
+      callback(customer);
+    
+  });
+};
+
