0aae8d3591294f8e14c48f77907ca2c90a82ebec
Matt Brown <mattfdbrown@gmail.com>
2015-05-10 13:43:46 -0600

    Add two function for account panel
    add the ability to get/set user data in database

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,36 @@ var router = express.Router();
 var User  = require('./../app/models/user');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+//router.get('/', function(req, res, next) {
+//  res.send('respond with a resource');
+//});
+
+router.post('/accountinfo', function(req, res, done) {
+  var currentUser = req.body.username;
+  new User({username: currentUser})
+    .fetch()
+    .then(function(user){
+      console.log('user',user);
+      res.json(user);
+    });
 });
 
+router.post('/accountupdate', function(req, res, done) {
+  console.log('updating account');
+  var currentUser = req.body.username;
+  var data = req.body;
+  new User({username: currentUser})
+    .fetch()
+    .then(function(user) {
+      console.log('data:',data);
+      user.save(data) 
+        .then(function() {
+          res.end('updated');
+        });
+    });
+});
+
+// Deprecated?
 router.post('/checkUser', function(req, res, done) {
   console.log(req.body,'req..body');
   var currentUser = req.body.username;
