db5fa111dbdff8acdb4dbe3b74553effd5bdfe41
Matt Brown <mattfdbrown@gmail.com>
2015-05-10 14:49:48 -0600

    resolve merge conflict

--- routes/users.js
+++ routes/users.js
@@ -8,21 +8,21 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/checkUser', function(req, res, done) {
-	console.log(req.body,'req..body');
+  console.log(req.body,'req..body');
   var currentUser = req.body.username;
   // check DB to see if current user has stripe information
   new User({username: currentUser})
-  	.fetch()
-  	.then(function(user){
-  		console.log('user',user);
-  		if (user.attributes.stripeId || currentUser === 'ggg' || currentUser === 'xxx'){
-  			// if user's stripe ID is present in DB
-  			res.json({hasStripeId: true});
-  		} else {
-  			console.log('StripeId not found for user');
-  			res.json({hasStripeId: false});
-  		}
-  	});
+    .fetch()
+    .then(function(user){
+      console.log('user',user);
+      if (user.attributes.stripeId || currentUser === 'ggg' || currentUser === 'xxx'){
+        // if user's stripe ID is present in DB
+        res.json({hasStripeId: true});
+      } else {
+        console.log('StripeId not found for user');
+        res.json({hasStripeId: false});
+      }
+    });
 });
 
 module.exports = router;
