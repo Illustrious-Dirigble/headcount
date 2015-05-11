1959caaedeb18d48e49c45f4ef47e8ed99a2e43f
Ansel Rosenberg <ansel01@gmail.com>
2015-05-10 22:56:33 -0700

    Change event payout function to handle test payouts as well as real payouts

--- utils/payments.js
+++ utils/payments.js
@@ -2,10 +2,10 @@ var qs = require('querystring');
 var request = require('request');
 var User = require('./../app/models/user.js');
 
-// var venmoEndpoint = 'https://api.venmo.com/v1/payments'
-var venmoEndpoint = 'https://sandbox-api.venmo.com/v1/payments';
+var venmoEndpoint;
 
 /**
+ * Subordinate function- only used in 'payOutEvent'.
  * Takes access token of of user who created event, user id of paying user, note (required, but can be any string), and amount (in dollars --> 1.50).
  * Returns payment information.
  */
@@ -20,7 +20,7 @@ var payEventCreator = function(payingAccessToken, receivingUserId, note, amount,
 
   request.post(payAuthorizedUserUrl, function (error, response, body) {
     if (error) {
-      console.log('Error:', error)
+      callback(error);
     } else {
       callback(body);
     }
@@ -28,7 +28,21 @@ var payEventCreator = function(payingAccessToken, receivingUserId, note, amount,
 
 };
 
-var payOutEvent = function(payingUserIds, receivingUserVenmoId, note, amount, callback) {
+/**
+ * Takens array of user IDs of paying users, the event creator's Venmo Id, auto-generated note, the amount each user is paying, boolean for testing purposes, and callback.
+ * If isTestBool is true, changes venmo endpoint sandbox url. Only works if correct payOutEvent function is uncommented in routes/index.js (the second one).
+ * Uses failsafeCounter to try to eliminate unwanted payments. User should not be able to commit to an event client-side without authorizing their Venmo account, but the 
+ * 'accessToken' conditional should prevent failed calls to Venmo API regardless. Calls calback on record of payments.
+ */
+var payOutEvent = function(payingUserIds, receivingUserVenmoId, note, amount, isTestBool, callback) {
+  if (isTestBool) {
+    console.log('Test payout!');
+    venmoEndpoint = 'https://sandbox-api.venmo.com/v1/payments';
+  } else {
+    venmoEndpoint = 'https://api.venmo.com/v1/payments';
+    console.log('Real payout!');
+  }
+
   var payments = [];
   var failsafeCounter = 0;
 
@@ -40,11 +54,16 @@ var payOutEvent = function(payingUserIds, receivingUserVenmoId, note, amount, ca
       }).fetch().then(function(user) {
 
         var accessToken = user.get('venmoAccessToken');
-        payEventCreator(accessToken, receivingUserVenmoId, note, amount, function(payment) {
-          payments.push(payment);
+        if (accessToken) {
+          payEventCreator(accessToken, receivingUserVenmoId, note, amount, function(payment) {
+            payments.push(payment);
+            failsafeCounter++;
+            inner();
+          });
+        } else {
           failsafeCounter++;
           inner();
-        })
+        }
       })
     } else {
       callback(payments);
