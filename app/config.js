3dfbde6cad99af35972f89831508f675d27d4515
Ansel Rosenberg <ansel01@gmail.com>
2015-05-11 00:31:42 -0700

    Clean up comments in config.js and user.js

--- app/config.js
+++ app/config.js
@@ -9,6 +9,9 @@ var knex =  !process.env.DATABASE_URL ? require('./local_config.js') :
 var db = require('bookshelf')(knex);
 db.plugin('registry');
 
+/**
+ * Columns email, firstName, lastName, shippingAddress and phoneNumber are currently not being used.
+ */
 db.knex.schema.hasTable('users').then(function(exists) {
   if (!exists) {
     db.knex.schema.createTable('users', function (user) {
