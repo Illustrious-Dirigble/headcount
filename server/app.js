2d91a5f7d6af5aace1fdb806376720f84362ab44
Khoa Phan <khoaphan89@gmail.com>
2015-05-12 09:52:23 -0700

    Add further comments to explain functionality and code clean-up

--- server/app.js
+++ server/app.js
@@ -33,7 +33,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Express-session settings
+// Express-session settings -- can be massively improved upon for better security/stability.
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
