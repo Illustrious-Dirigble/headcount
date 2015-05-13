fc6689cbc89e4755d22be234cc97b5c0ae0b2c17
TheWhiteOx <thewhiteoxinbox@gmail.com>
2015-05-13 16:38:07 -0700

    secure venmo keys

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var qs = require('querystring');
 var request = require('request');
+var oauth = require('./../oauth');
 
 var payOutEvent = require('./../utils/payments.js');
 var attendance = require('./../utils/invites.js');
@@ -257,7 +258,7 @@ router.post('/events-create', function(req, res) {
 router.post('/authorize', function(req, res) {
 
   var username = req.body.username;
-  var clientId = '2612';
+  var clientId = oauth.ids.venmo.clientID;
   var scopes = 'make_payments%20access_feed%20access_profile%20access_email%20access_phoneaccess_balance%20access_friends';
 
   var redirect_uri = !process.env.DATABASE_URL ? 'http://localhost:5000/oauth' :
@@ -276,8 +277,8 @@ router.post('/authorize', function(req, res) {
  */
 router.get('/oauth', function(req, res) {
   var venmoTokenUri = 'https://api.venmo.com/v1/oauth/access_token';
-  var clientId = '2612';
-  var clientSecret = 'eUv3N6JDsM3YCGkzmF8Lg8kH9WtV6kuf';
+  var clientId = oauth.ids.venmo.clientID;
+  var clientSecret = oauth.ids.venmo.clientSecret;
   var username = req.query.state;
   var code = req.query.code;
 
